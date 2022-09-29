0beb513f4ed68800065b1b956dcf5a76c3b56978
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-09-29 15:20:13 +0100

    PASClickUp

--- Part 0/0.4: New note/main.js
+++ Part 0/0.4: New note/main.js
@@ -3,8 +3,11 @@ let space = document.querySelector('#space')
 let save = document.querySelector('#save')
 let newLi
 
+
 save.addEventListener('click', (e) => {
     newLi = document.createElement('li')
     newLi.textContent = space.value
+
     ul.append(newLi)
+
 })
