ce33707c86a6435cee813896583178f32ebf9147
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-10-04 11:19:44 +0100

    PASClickUp

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,33 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old, your country is {props.country}.
+      </p>
+    </div>
+  )
+}
 
+const footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  const country = 'Nigeria'
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} country='Togo' />
+      <Hello name={name} age={age} country={country} />
+      <Hello age='54' country='Kenya' />
+      <footer />
+    </div>
+  )
+}
 export default App
