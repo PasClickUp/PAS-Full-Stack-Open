c11a0c9830002b2fe14d24a8562ffffd1d6f2c29
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-10-04 15:01:36 +0100

    PASClickUp

--- part1/course information/step1/src/App.js
+++ part1/course information/step1/src/App.js
@@ -1,27 +1,38 @@
+const exercises1 = 10
+const exercises2 = 7
+const exercises3 = 14
 
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+const Header = () => {
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h1>Half Stack application development</h1>
+    </div>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <p>Fundamentals of React {exercises1}</p>
+      <p>Using props to pass data {exercises2}</p>
+      <p>State of a component {exercises3}</p>
+    </div>
+  )
+}
+
+const Total = () => {
+  return (
+    <div>
+      <h3>Total Number of exercises is {exercises1 + exercises2 + exercises3}</h3>
+    </div>
+  )
+}
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
     </div>
   )
 }
