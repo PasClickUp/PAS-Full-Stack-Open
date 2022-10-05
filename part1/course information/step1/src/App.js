d258dae9b2f88a7cfb9885de388414d9efeb8a34
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-10-05 17:30:43 +0100

    PASClickUp

--- part1/course information/step1/src/App.js
+++ part1/course information/step1/src/App.js
@@ -1,42 +1,42 @@
-const course = 'Half Stack application development'
-const part1 = 'Fundamentals of React'
-const exercises1 = 10
-const part2 = 'Using props to pass data'
-const exercises2 = 7
-const part3 = 'State of a component'
-const exercises3 = 14
-
-const Header = () => {
+const Header = (props) => {
   return (
     <div>
-      <h1>{course}</h1>
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-const Content = () => {
+const Content = (props) => {
   return (
     <div>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
+      <p>Part 1: {props.part1} {props.exercises1}</p>
+      <p>Part 2: {props.part2} {props.exercises2}</p>
+      <p>Part 3: {props.part3} {props.exercises3}</p>
     </div>
   )
 }
 
-const Total = () => {
+const Total = (props) => {
   return (
     <div>
-      <h3>Total Number of exercises is {exercises1 + exercises2 + exercises3}</h3>
+      <h3>Total Number of exercises is {props.exercises1 + props.exercises2 + props.exercises3}</h3>
     </div>
   )
 }
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
