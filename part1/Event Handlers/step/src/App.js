39c68590af6cbb27a21f03bf6792d7db1721d540
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-10-12 15:52:25 +0100

    PASClickUp

--- part1/Event Handlers/step/src/App.js
+++ part1/Event Handlers/step/src/App.js
@@ -1,27 +1,8 @@
-const Hello = ({ name, age, love }) => {
-
-  const bornYear = () => new Date().getFullYear() - age
-
+const App = (props) => {
+  const { counter } = props
   return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}, you really love {love}.</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-  const love = 'sleeping'
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="YOU" age={26 + 10} love={love} />
-
-      <Hello name={name} age={age} love='playing' />
-    </div>
-  )
-}
-
-export default App;
+export default App
