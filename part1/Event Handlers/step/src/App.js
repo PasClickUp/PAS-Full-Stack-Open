b4bbc07492946d8e668f7decc8d17d8b9b5922fe
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-10-13 10:51:57 +0100

    PASClickUp

--- part1/Event Handlers/step/src/App.js
+++ part1/Event Handlers/step/src/App.js
@@ -1,16 +1,28 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [counter, setCounter] = useState(89)
+  const [counter, setCounter] = useState(0)
+  // const handleClick = () => {
+  //   console.log('clicked')
+  // }
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  console.log('rendering...', counter)
+  // return (
+  //   <div>
+  //     <div>{counter}</div>
+  //     <button onClick={handleClick}>
+  //       plus
+  //     </button>
+  //   </div>
+  // )
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => console.log('clicked')}>
+        plus
+      </button>
+    </div>
   )
+
 }
 
 export default App
