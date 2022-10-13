68a6ac91aee8c283a4a146b59824bd53211f8dff
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-10-13 10:39:53 +0100

    PASClickUp

--- part1/Event Handlers/step/src/index.js
+++ part1/Event Handlers/step/src/index.js
@@ -3,21 +3,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <App counter={counter} />
-// )
-
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-
-
-counter += 1
-counter += 1
-// refresh()
-counter += 7
-refresh()
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
