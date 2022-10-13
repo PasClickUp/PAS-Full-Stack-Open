5bf45c1932d96d311ad90e8e95440cf0a8a3820c
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-10-13 14:28:41 +0100

    PASClickUp

--- part1/Event Handlers/step/src/App.js
+++ part1/Event Handlers/step/src/App.js
@@ -1,25 +1,43 @@
 import { useState } from 'react'
-const Display = ({ counter }) => <div>{counter}</div>
+// const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text} </button>
+// const Button = ({ onClick, text }) => <button onClick={onClick}>{text} </button>
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1)
+//   const decreaseByOne = () => setCounter(counter - 1)
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+
+//       <Button onClick={increaseByOne} text="Increase" />
+//       <Button onClick={decreaseByOne} text="Decrease" />
+//       <Button onClick={setToZero} text="Restart" />
+//     </div>
+//   )
+
+// }
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
   return (
     <div>
-      <Display counter={counter} />
-
-      <Button onClick={increaseByOne} text="Increase" />
-      <Button onClick={decreaseByOne} text="Decrease" />
-      <Button onClick={setToZero} text="Restart" />
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
   )
-
 }
 
 export default App
