1fee1838e397f17b825eeebd495b99b452d7f7b4
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-10-17 15:39:20 +0100

    PASClickUp

--- part1/Event Handlers/step/src/App.js
+++ part1/Event Handlers/step/src/App.js
@@ -1,26 +1,4 @@
 import { useState } from 'react'
-// const Display = ({ counter }) => <div>{counter}</div>
-
-// const Button = ({ onClick, text }) => <button onClick={onClick}>{text} </button>
-
-// const App = () => {
-//   const [counter, setCounter] = useState(0)
-
-//   const increaseByOne = () => setCounter(counter + 1)
-//   const decreaseByOne = () => setCounter(counter - 1)
-//   const setToZero = () => setCounter(0)
-
-//   return (
-//     <div>
-//       <Display counter={counter} />
-
-//       <Button onClick={increaseByOne} text="Increase" />
-//       <Button onClick={decreaseByOne} text="Decrease" />
-//       <Button onClick={setToZero} text="Restart" />
-//     </div>
-//   )
-
-// }
 
 const App = () => {
   const [clicks, setClicks] = useState({
@@ -29,15 +7,15 @@ const App = () => {
 
   const handleLeftClick = () => {
     const newClicks = {
-      left: clicks.left + 1,
-      right: clicks.right
+      ...clicks,
+      left: clicks.left + 1
     }
     setClicks(newClicks)
   }
 
   const handleRightClick = () => {
     const newClicks = {
-      left: clicks.left,
+      ...clicks,
       right: clicks.right + 1
     }
     setClicks(newClicks)
