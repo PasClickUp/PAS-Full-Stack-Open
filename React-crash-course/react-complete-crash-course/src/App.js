7d3d0689562bb77cb1ca21ee8e95d8dcb3f84593
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-12-09 11:41:04 +0100

    PASClickUp

--- React-crash-course/react-complete-crash-course/src/App.js
+++ React-crash-course/react-complete-crash-course/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'M a React App</h1>
+      <p>This is really working!</p>
+      <Person name="Baaqi" age="7" />
+      <Person name="Fulaan" age="9">My Hobbies: eating</Person>
+      <Person name="Walad" age="11" />
     </div>
+    // React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this make sense now?'))
   );
 }
 
