02f98e24f91cccd6f042dd53efe2c80a93a23b1f
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-12-09 16:47:43 +0100

    PASClickUp

--- React-crash-course/react-complete-crash-course/src/App.js
+++ React-crash-course/react-complete-crash-course/src/App.js
@@ -1,30 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = () => {
-  let state = {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Baaqi', age: 7 },
       { name: 'Fulaan', age: 9 },
       { name: 'Walad', age: 11 }
     ]
-  }
+  });
 
-  let switchNameHandler = () => {
-    console.log('Was Clicked')
-  }
+  const [otherState, setOtherState] = useState('some other value')
+
+  console.log(personsState, otherState)
+  const switchNameHandler = () => {
+    // console.log('Was Clicked')
+    // state.persons[0] = 'Baaqi is my brother'
+    setPersonsState({
+      persons: [
+        { name: 'Baaqi my brother', age: 7 },
+        { name: 'Fulaan', age: 9 },
+        { name: 'Walad', age: 11 }
+      ],
+      otherState: personsState.otherState
+    });
+  };
+
   return (
     <div className="App">
       <h1>Hi, I'M a React App</h1>
       <p>This is really working!</p>
       <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={state.persons[0].name} age={state.persons[0].age} />
-      <Person name={state.persons[1].name} age={state.persons[1].age}>My Hobbies: eating</Person>
-      <Person name={state.persons[2].name} age={state.persons[2].age} />
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: eating</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
     </div>
     // React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this make sense now?'))
   );
+
 }
 
 export default App;
