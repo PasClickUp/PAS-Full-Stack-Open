666a7911c79e7bab389d203401bd3c10a7e7aac4
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-12-12 15:42:19 +0100

    PASClickUp

--- React-crash-course/react-complete-crash-course/src/App.js
+++ React-crash-course/react-complete-crash-course/src/App.js
@@ -20,25 +20,35 @@ class App extends Component {
       persons: [
         { name: newName, age: 7 },
         { name: 'Fulaan', age: 9 },
-        { name: 'Walad', age: 11 }
+        { name: 'Walad', age: 18 }
       ],
-      // otherState: personsState.otherState
     });
   };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Imraan', age: 17 },
+        { name: event.target.value, age: 9 },
+        { name: 'Walad', age: 26 }
+      ],
+    });
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'M a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Baaqi Mi')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}>My Hobbies: eating</Person>
+          click={this.switchNameHandler.bind(this, 'My brother Baaqi!')}
+          changed={this.nameChangedHandler}>My Hobbies: eating</Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age} />
